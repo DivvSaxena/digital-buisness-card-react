165084fed0ef996264a117f64893548caad98ca6
Divv Saxena <saxenadivv@gmail.com>
2023-09-25 19:31:50 +0530

    add this

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer(){
+    return (
+        <div className="footer-section">
+            <div className='icons-club'>
+                <a href="https://twitter.com/Divvstwt"><i class="fa-brands fa-square-twitter"></i></a>
+                <a href=""><i class="fa-brands fa-square-facebook"></i></a>
+                <a href="https://www.instagram.com/divvsaxena/"><i class="fa-brands fa-square-instagram"></i></a>
+                <a href="https://github.com/DivvSaxena"><i class="fa-brands fa-square-github"></i></a>
+            </div>
+        </div>
+    )
+}
